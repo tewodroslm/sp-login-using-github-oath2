b002f4739e0516fb6be4cf62dd75c99b6471c6ed
Mamushe <tewodroslemma64@gmail.com>
2023-06-05 15:41:43 -0400

    simple get

---
+++ src/main/java/com/ted/spauthaws/spauthAwsElastic/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.ted.spauthaws.spauthAwsElastic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "hello"})
+    public String getHome(@RequestParam(required = false, defaultValue = "World") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-world";
+    }
+
+}
