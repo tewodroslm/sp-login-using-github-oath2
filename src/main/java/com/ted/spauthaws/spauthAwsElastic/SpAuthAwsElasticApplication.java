3099a172f02cd5e2c68d6dec499f8ae08fcfe1af
Mamushe <tewodroslemma64@gmail.com>
2023-06-05 15:13:50 -0400

    initiated

---
+++ src/main/java/com/ted/spauthaws/spauthAwsElastic/SpAuthAwsElasticApplication.java
@@ -0,0 +1,13 @@
+package com.ted.spauthaws.spauthAwsElastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAuthAwsElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpAuthAwsElasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ted/spauthaws/spauthAwsElastic/SpAuthAwsElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ted.spauthaws.spauthAwsElastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpAuthAwsElasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
